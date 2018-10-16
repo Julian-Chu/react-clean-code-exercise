9ed03e3711ba5521a945a022d1f7dfcf09091104
l7960261 <l7960261@gmail.com>
2018-10-16 17:11:58 +0800

    Inline temporary variables

--- src/domain/budget.js
+++ src/domain/budget.js
@@ -21,8 +21,7 @@ export class Budget {
         const monthString = moment(startDate, 'YYYY-MM-DD')
           .add(month, 'month')
           .format('YYYY-MM')
-        const budgetThisMonth = this.budgets[monthString] || 0
-        budget += budgetThisMonth
+        budget += this.budgets[monthString] || 0
       }
 
       // end month
@@ -35,7 +34,6 @@ export class Budget {
       const diffDays = momentEndDate.diff(momentStartDate, 'days') + 1
       let dayCountOfFirstBudget = momentStartDate.daysInMonth();
       let amountOfFirstBudget = (this.budgets[momentStartDate.format('YYYY-MM')] || 0);
-      const budget = amountOfFirstBudget / dayCountOfFirstBudget * diffDays
-      return budget;
+      return amountOfFirstBudget / dayCountOfFirstBudget * diffDays;
   }
 }
