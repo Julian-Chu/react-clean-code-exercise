bfbd8559db7a04a19d32c6b2a97b296bb953757e
l7960261 <l7960261@gmail.com>
2018-10-16 14:57:06 +0800

    modify profile.spec for updateProfile

--- src/tests/components/profile.spec.js
+++ src/tests/components/profile.spec.js
@@ -5,39 +5,14 @@ import Api from "../../api";
 
 
 describe('profile', () => {
-    let wrapper;
-    let _store = {};
-    global.localStorage = {
-        getItem: key => {
-            return _store[key];
-        },
-        setItem: (key, input) => {
-            _store[key] = input;
-        }
-    };
-
-    beforeEach(() => {
-        Api.getProfile = jest.fn().mockReturnValue(null);
-        // Api.updateProfile = jest.fn();
-        wrapper = shallow(<Profile/>);
-    });
-    afterEach(() => {
-        _store = {};
-    });
-
-    it('default value', () => {
-        expect(wrapper.state().profile.name).toEqual('');
-        expect(wrapper.state().profile.birthday.year).toEqual(1980);
-        expect(wrapper.state().profile.birthday.month).toEqual(1);
-        expect(wrapper.state().profile.birthday.day).toEqual(1);
-    });
 
     it('save', () => {
+        const profile = {name: 'Wilson', birthday: { year: 1990, month: 6, day: 26}};
+        Api.getProfile = jest.fn().mockReturnValue(profile);
+        Api.updateProfile = jest.fn();
+
+        const wrapper = shallow(<Profile/>);
         wrapper.instance().save();
-        // expect(Api.updateProfile.mock.calls.length).toBe(1);
-        const result = JSON.parse(_store['profile']);
-        expect(result.birthday.year).toEqual(1980);
-        expect(result.birthday.month).toEqual(1);
-        expect(result.birthday.day).toEqual(1);
+        expect(Api.updateProfile).toHaveBeenCalledWith(profile);
     });
 });
