5e294e2d8ea94113de699e74b04630a2cccd6a55
l7960261 <l7960261@gmail.com>
2018-10-17 15:54:10 +0800

    add Validator.js

--- src/domain/AddBudgetAction.js
+++ src/domain/AddBudgetAction.js
@@ -1,41 +1,28 @@
 import Api from "../api";
+import {FORMAT, NOT_EMPTY, POSITIVE_NUMBER, Validator} from "./Validator";
 
 export default class AddBudgetAction {
-    do(budget, success, failure) {
-        let errors = {}
-        let monthValid, amountValid
-        if (budget.month === '') {
-            errors.month = 'Month cannot be empty'
-            monthValid = false
-        } else if (!(/^\d{4}-\d{2}$/g).test(budget.month)) {
-            errors.month = 'Invalid month format'
-            monthValid = false
-        } else {
-            errors.month = ''
-            monthValid = true
-        }
-        if (budget.amount === '') {
-            errors.amount = 'Amount cannot be empty'
-            amountValid = false
-        } else if (isNaN(parseInt(budget.amount, 10)) || budget.amount < 0) {
-            errors.amount = 'Invalid amount'
-            amountValid = false
-        } else {
-            errors.amount = ''
-            amountValid = true
-        }
-        if (!monthValid || !amountValid) {
-            failure(errors)
-            return
-        }
-        let budgets = Api.getBudgets()
-        let existing = budgets && budgets.find(existedBudget => existedBudget.month === budget.month)
-        if (existing) {
-            Api.updateBudget(budget)
-        } else {
-            Api.addBudget(budget)
-        }
-        // this._goToBudgetList()
-        success()
+  do(budget, success, failure) {
+    let errors = {}
+
+    let validator = new Validator({
+      month: [NOT_EMPTY, FORMAT],
+      amount: [NOT_EMPTY, POSITIVE_NUMBER]
+    })
+    validator.validate(budget, (field, error) => errors[field] = error)
+
+    if (!validator.valid) {
+      failure(errors)
+      return
     }
+    let budgets = Api.getBudgets()
+    let existing = budgets && budgets.find(existedBudget => existedBudget.month === budget.month)
+    if (existing) {
+      Api.updateBudget(budget)
+    } else {
+      Api.addBudget(budget)
+    }
+    // this._goToBudgetList()
+    success()
+  }
 }
---
+++ src/domain/Validator.js
@@ -0,0 +1,30 @@
+import _, {upperFirst} from "lodash";
+
+export const notEmpty = value => value !== ''
+export const emptyError = field => `${upperFirst(field)} cannot be empty`
+export const format = value => (/^\d{4}-\d{2}$/g).test(value)
+export const formatError = field => `Invalid ${field} format`
+export const positiveError = field => `Invalid ${field}`
+export const positiveNumber = value => !isNaN(parseInt(value, 10)) && value >= 0
+export const NOT_EMPTY = {validate: notEmpty, error: emptyError}
+export const FORMAT = {validate: format, error: formatError}
+export const POSITIVE_NUMBER = {validate: positiveNumber, error: positiveError}
+
+export class Validator {
+  errors = {}
+  constructor(rules){
+    this.rules = rules
+  }
+
+  validate(data, callback) {
+    for (let field in this.rules) {
+      let fail = this.rules[field].find(validation => !validation.validate(data[field])) || {error: () => ''}
+      callback(field, fail.error(field))
+      this.errors[field] = fail.error(field)
+    }
+  }
+
+  get valid() {
+    return _.chain(this.errors).values().compact().isEmpty().value();
+  }
+}
