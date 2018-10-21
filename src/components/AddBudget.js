32cc5c7bc370eb873d819fc730456f8fa14a3e30
JulianChu <yulang.chu@gmail.com>
2018-10-22 00:01:59 +0800

    extract save()

--- src/components/AddBudget.js
+++ src/components/AddBudget.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Api from "../api";
+import AddBudgetAction from "../domain/AddBudgetAction";
 
 export default class AddBudget extends React.Component {
   state = {
@@ -17,41 +18,43 @@ export default class AddBudget extends React.Component {
   }
 
   save() {
-    let {budget} = this.state
-    let errors = {}
-    let monthValid, amountValid
-    if (budget.month === '') {
-      errors.month = 'Month cannot be empty'
-      monthValid = false
-    } else if (!(/^\d{4}-\d{2}$/g).test(budget.month)) {
-      errors.month = 'Invalid month format'
-      monthValid = false
-    } else {
-      errors.month = ''
-      monthValid = true
-    }
-    if (budget.amount === '') {
-      errors.amount = 'Amount cannot be empty'
-      amountValid = false
-    } else if (isNaN(parseInt(budget.amount, 10)) || budget.amount < 0) {
-      errors.amount = 'Invalid amount'
-      amountValid = false
-    } else {
-      errors.amount = ''
-      amountValid = true
-    }
-    if (!monthValid || !amountValid) {
-      this.setState({errors})
-      return
-    }
-    let budgets = Api.getBudgets()
-    let existing = budgets && budgets.find(existedBudget => existedBudget.month === budget.month)
-    if (existing) {
-      Api.updateBudget(budget)
-    } else {
-      Api.addBudget(budget)
-    }
-    this._goToBudgetList()
+      let action = new AddBudgetAction();
+      action.do(this.state.budget,()=>this._goToBudgetList() , errors=>this.setState({errors}));
+    // let {budget} = this.state
+    // let errors = {}
+    // let monthValid, amountValid
+    // if (budget.month === '') {
+    //   errors.month = 'Month cannot be empty'
+    //   monthValid = false
+    // } else if (!(/^\d{4}-\d{2}$/g).test(budget.month)) {
+    //   errors.month = 'Invalid month format'
+    //   monthValid = false
+    // } else {
+    //   errors.month = ''
+    //   monthValid = true
+    // }
+    // if (budget.amount === '') {
+    //   errors.amount = 'Amount cannot be empty'
+    //   amountValid = false
+    // } else if (isNaN(parseInt(budget.amount, 10)) || budget.amount < 0) {
+    //   errors.amount = 'Invalid amount'
+    //   amountValid = false
+    // } else {
+    //   errors.amount = ''
+    //   amountValid = true
+    // }
+    // if (!monthValid || !amountValid) {
+    //   this.setState({errors})
+    //   return
+    // }
+    // let budgets = Api.getBudgets()
+    // let existing = budgets && budgets.find(existedBudget => existedBudget.month === budget.month)
+    // if (existing) {
+    //   Api.updateBudget(budget)
+    // } else {
+    //   Api.addBudget(budget)
+    // }
+    // this._goToBudgetList()
   }
   cancel(){
     this._goToBudgetList()
