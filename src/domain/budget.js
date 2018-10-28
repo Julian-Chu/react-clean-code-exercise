66881e1365dbcbe45f92e55a3fcec0618803b557
JulianChu <yulang.chu@gmail.com>
2018-10-28 14:52:12 +0100

    create class Period

--- src/domain/budget.js
+++ src/domain/budget.js
@@ -1,31 +1,42 @@
 import moment from 'moment'
 
+class Period {
+    startDate;
+    endDate;
+
+    constructor(startDate, endDate) {
+        this.startDate = moment(startDate, 'YYYY-MM-DD');
+        this.endDate = moment(endDate, 'YYYY-MM-DD');
+    }
+
+}
+
 export class Budget {
     budgets = {};
 
     query(startDate, endDate) {
-        const momentStartDate = moment(startDate, 'YYYY-MM-DD');
-        const momentEndDate = moment(endDate, 'YYYY-MM-DD');
-
-        if (momentStartDate.isSame(momentEndDate, 'month')) {
-            return this.getAmountByPeriod(momentStartDate, momentEndDate);
+        // const momentStartDate = moment(startDate, 'YYYY-MM-DD');
+        // const momentEndDate = moment(endDate, 'YYYY-MM-DD');
+        const period = new Period(startDate, endDate);
+        if (period.startDate.isSame(period.endDate, 'month')) {
+            return this.getAmountByPeriod(period.startDate, period.endDate);
         } else {
             let budget = 0;
 
             // start month
-            budget += this.getAmountByPeriod(momentStartDate, moment(momentStartDate).endOf("month"));
+            budget += this.getAmountByPeriod(period.startDate, moment(period.startDate).endOf("month"));
 
             // months in between
-            const monthDiff = momentEndDate.diff(momentStartDate, 'months') - 1;
+            const monthDiff = period.endDate.diff(period.startDate, 'months') - 1;
             for (let month = 1; month <= monthDiff; month++) {
-                const monthString = moment(momentStartDate, 'YYYY-MM-DD')
+                const monthString = moment(period.startDate, 'YYYY-MM-DD')
                     .add(month, 'month')
                     .format('YYYY-MM');
                 budget += this.budgets[monthString] || 0;
             }
 
             // end month
-            budget += this.getAmountByPeriod(moment(momentEndDate).startOf("month"), momentEndDate);
+            budget += this.getAmountByPeriod(moment(period.endDate).startOf("month"), period.endDate);
             return budget
         }
     }
